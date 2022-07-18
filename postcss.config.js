72267787f2a0f9a3aa06b35ed655fe1145b2ec83
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-07-18 11:56:29 +0200

    Theme compilation

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    'postcss-nested': {},
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['**/*.twig'],
+  separator: '§',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#279e95',
+      },
+      width: {
+        '88': '22rem',
+        '92': '23rem',
+      },
+      inset: {
+        '370px': '370px',
+        '368px': '-368px',
+        '680px': '680px',
+      },
+      screens: {
+        xs2: '240px',
+        sm: '340px',
+        sm2: '640px,',
+      },
+    },
+  },
+}
